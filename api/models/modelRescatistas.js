2c1ea5dbf07a725d19e15c329f26e7a23c2ca2f1
solmsl <mejiasilesol@gmail.com>
2024-10-03 15:10:04 -0300

    validaciones en el modelo de rescatistas.

--- api/models/modelRescatistas.js
+++ api/models/modelRescatistas.js
@@ -11,13 +11,12 @@ const db = require('../config/db');
  * @property {string} genero - género del rescatista.
  * @property {string} email - El correo electronico del rescatista.
  * @property {string} passw - contraseña de la cuenta del rescatista.
- * @property {string} re_passw - confirmar contraseña de la cuenta del rescatista.
  */
 
 // Definir la estructura de la tabla rescatista
 const Rescatista = db.define('rescatistas', {
   dni: {
-    type: Sequelize.INTEGER(8),
+    type: Sequelize.INTEGER,
     primaryKey: true,
     allowNull: false,
     unique: true,
@@ -26,108 +25,127 @@ const Rescatista = db.define('rescatistas', {
         msg: 'El DNI debe ser un número entero'
       },
       notEmpty: { 
-        msg: 'Este campo No puede estar vacio' 
+        msg: 'Este campo no puede estar vacío' 
       },
       validarDni(value) {
-        if (!value || value.length < 8) {
-          throw new Error('DNI inválido! Debe tener al menos 8 digitos.');
+        const dniString = String(value);
+        if (dniString.length < 8) {
+          throw new Error('DNI inválido: debe tener al menos 8 dígitos.');
         }
-        if (value.length > 8){
-          throw new Error('DNI inválido! El dni tiene demasiados dígitos.')
+        if (dniString.length > 8) {
+          throw new Error('DNI inválido: tiene demasiados dígitos.');
         }
       }
     }
   },
   nombre: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-          len: {
-            args: [3,],
-            msg: 'Nombre inválido: debe tener mas de 3 caracteres'
-          },
-          notEmpty: true
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [2],
+        msg: 'Nombre inválido: debe tener más de 2 caracteres'
+      },
+      notEmpty: {
+        msg: 'Este campo no puede estar vacío'
+      },
+      isAlpha: {
+        msg: 'El nombre solo debe contener letras'
       }
+    }
   },
   apellido: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-          len: {
-            args: [3,],
-            msg: 'Apellido inválido: debe tener mas de 3 caracteres'
-          },
-          notEmpty: true
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [2],
+        msg: 'Apellido inválido: debe tener más de 2 caracteres'
+      },
+      notEmpty: {
+        msg: 'Este campo no puede estar vacío'
+      },
+      isAlpha: {
+        msg: 'El apellido solo debe contener letras'
       }
-    
+    }
   },
   telefono: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      validate: {
-        isNumeric: {
-          msg: 'El teléfono debe contener solo números'
-        },
-        len: {
-          args: [7, 15],
-          msg: 'Número de Teléfono inválido: debe tener entre 7 y 15 caracteres'
-        },
-        notEmpty: true
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isNumeric: {
+        msg: 'El teléfono debe contener solo números'
+      },
+      len: {
+        args: [7, 15],
+        msg: 'Número de Teléfono inválido: debe tener entre 7 y 15 caracteres'
+      },
+      notEmpty: {
+        msg: 'Este campo no puede estar vacío'
       }
+    }
   }, 
   direccion: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          msg: 'Este campo no puede estar vacío'
-        },
-        len: {
-          args: [0, 255],
-          msg: 'Dirección inválida: máximo 255 caracteres'
-        }
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      is: {
+        args: /^[a-zA-Z0-9\s,.#-]+$/i,
+        msg: 'La dirección contiene caracteres no permitidos'
+      },
+      notEmpty: {
+        msg: 'Este campo no puede estar vacío'
+      },
+      len: {
+        args: [1, 255],
+        msg: 'Dirección inválida: máximo 255 caracteres'
       }
+    }
   },
   genero: {
-      type: Sequelize.STRING,
-      allowNull: true,
-      validate: {
-        isIn: {
-          args: [['M', 'F', 'Otro']],
-          msg: 'Género inválido: debe ser Masculino, Femenino u Otro'
-        }
+    type: Sequelize.STRING,
+    allowNull: true,
+    validate: {
+      isIn: {
+        args: [['M', 'F', 'Otro']],
+        msg: 'Género inválido: debe ser Masculino, Femenino u Otro'
       }
+    }
   },
   email: {
     type: Sequelize.STRING,
     unique: true,
     allowNull: false,
     validate: {
-        isEmail: true,
-        notEmpty: true
+      isEmail: {
+        msg: 'El correo electrónico no es válido'
+      },
+      notEmpty: {
+        msg: 'Este campo no puede estar vacío'
+      }
     }
   },
   passw: {
     type: Sequelize.STRING,
-    allowNull:false,
+    allowNull: false,
     validate: {
       notEmpty: {
         msg: 'Este campo no puede estar vacío'
       },
+      len: {
+        args: [8, 100],
+        msg: 'La contraseña debe tener al menos 8 caracteres'
+      },
+      is: {
+        args: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/,
+        msg: 'La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial'
+      }
     }
-  },
-  // re_passw: {
-  //   type: Sequelize.STRING,
-  //   allowNull:false,
-  //   validate: {
-  //     notEmpty: {
-  //       msg: 'Este campo no puede estar vacío'
-  //     },
-  //   }
-  // }
+  }
 }, {
   timestamps: false
-})
+});
 
 Rescatista.sync({ force: false })
   .then(() => {
@@ -135,7 +153,6 @@ Rescatista.sync({ force: false })
   })
   .catch(err => {
     console.error('Error al sincronizar el Modelo de Rescatista:', err);
-  }
-);
+  });
 
 module.exports = Rescatista;
